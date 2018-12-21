dcec8f57c110470d4cbea58bd671cc9f69b1ff98
hahehuheho <hahehuheho@hanmail.net>
2018-12-21 15:13:15 +0900

    readme

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import { getTimeFieldValues } from 'uuid-js';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>No More Error</Text>
-      </View>
-    );
+class App extends Component {
+  state = {
+    isLoaded:getTimeFieldValues
+  };
+
+  render(){
+    const{isLoaded}=this.state;
+    return(
+        <View>
+          {isLoaded ? null : (
+            <View>
+              <Text>Getting your Weather~!
+              </Text>
+          
+            </View>
+          )}
+        </View>
+    )
+
   }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>No More Error</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
